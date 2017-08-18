cf913372a46cd3bc75bbbbe8fa1f1d95ce482d4a
wushaobo <wushaobo.china@gmail.com>
2017-08-18 10:05:53 +0800

    update dockerfile for xtrabackup

--- apply_mysql_binlog.sh
+++ apply_mysql_binlog.sh
@@ -2,11 +2,11 @@
 
 CMD_NAME=$(basename $0)
 MYSQL_FOLDER=$1
+IMAGE=wushaobo/percona-xtrabackup
 
 if [ ! -d "${MYSQL_FOLDER}" ]; then
 	echo "Usage: ${CMD_NAME} /ABSOLUTE/PATH/TO/MYSQL_SNAPSHOT/FOLDER"
 	exit 1
 fi
 
-docker run --name mysql_tmp -v ${MYSQL_FOLDER}:/data/mysql daocloud.io/shaobo_wu/percona-binlog-recover innobackupex --apply-log /data/mysql/
-docker rm mysql_tmp
+docker run --rm --name mysql_tmp -v ${MYSQL_FOLDER}:/data/mysql ${IMAGE} innobackupex --apply-log /data/mysql/
